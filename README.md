e2a4535586744375779ac1207c58de4ad1c1ffb3
Ronesha Dennis <ronesha_dennis@yahoo.com>
2023-04-05 00:53:55 -0400

    commiting files for push to github

---
+++ README.md
@@ -0,0 +1,25 @@
+# CODEPATH: Student Registration Starter Code
+
+This is the starter code that can be used to build the Student Registration Application template.
+
+
+## Getting Started
+
+### Dependencies
+
+* Node
+
+### Executing program
+
+```
+npm start
+```
+
+### For Continued Learning
+
+Find a course list in resource_files/courses.txt, which can be used for the continued learning tasks
+
+## Authors
+
+Ronesha D. Dennis
+[@RoneshaDDennis](https://linkedin.com/in/roneshaddennis)
---
+++ src/form.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const Form = () => {
+
+
+    return (
+        <div className="container">
+            <div className="row">
+                {/* COLUMN ONE */}
+                    <div className="col">
+                        {/* Insert Code For Column 1 (Form) */}
+                    <form>
+
+                    </form>
+                    </div>
+                {/* END OF COLUMN ONE */}
+
+                {/* COLUMN TWO */}
+                <div className="col bg-light ps-5 pt-5">
+                    <p className="lead">
+                        {/* Insert Code Here for Column 2 (Output Validation) */}
+
+
+                    </p>
+                </div>
+                {/* END OF COLUMN TWO */}
+            </div>
+        </div>
+    )
+
+}
+
+export default Form;
